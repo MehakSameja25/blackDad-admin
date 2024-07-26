b7fad08ad430361af12e85d3a558cc030da56182
jainv3905 <jainv3905@gmail.com>
2024-07-26 16:10:04 +0530

    add notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminAuthComponent } from './admin/admin-auth/admin-auth.component';
@@ -99,6 +99,7 @@ import { SimpleNotificationsModule } from 'angular2-notifications';
     CKEditorModule,
     ImageCropperComponent,
     NgMultiSelectDropDownModule.forRoot(),
+    BrowserAnimationsModule,
     SimpleNotificationsModule.forRoot({
       timeOut: 3500,
       showProgressBar: true,
