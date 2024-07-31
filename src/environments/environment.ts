c4ff43586dbf1069fee4b6cf8e5b0fc255172e58
palNeelam <palneelam13@gmail.com>
2024-07-31 15:54:31 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://213.210.21.175:4100/api/v1',
+  apiUrl: 'https://213.210.21.175:4100/api/v1',
   shareUrl: 'https://www.blackdadunfiltered.com/details',
-  nullImagePath: 'http://213.210.21.175:4100/api/v1/null',
+  nullImagePath: 'https://213.210.21.175:4100/api/v1/null',
 };
