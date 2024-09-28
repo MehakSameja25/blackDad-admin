0c03104dbf5f1c2cfdc5c42d1bce6544229951bf
jainv3905 <jainv3905@gmail.com>
2024-09-28 12:57:29 +0530

    merge with staging

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,12 @@
 export const environment = {
-  apiUrl: 'https://api.blackdad.chooseyourfabric.in/api/v1',
-  shareUrl: 'https://www.blackdadunfiltered.com/details',
-  nullImagePath: 'https://api.blackdad.chooseyourfabric.in/api/v1/null',
+
+  //stagging url
+  // apiUrl: 'https://api.blackdad.chooseyourfabric.in/api/v1',
+  // shareUrl: 'https://www.blackdadunfiltered.com/details',
+  // nullImagePath: 'https://api.blackdad.chooseyourfabric.in/api/v1/null',
+
+  //live url
+  apiUrl: 'https://api.blackdadunfiltered.com/api/v1',
+  shareUrl: 'https://blackdadunfiltered.com/details',
+  nullImagePath: "'https://api.blackdadunfiltered.com/api/v1/null"
 };
