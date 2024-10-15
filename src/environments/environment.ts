21d461380608e5bb6a53cc9025ff1d6e643ef4f7
MehakSameja25 <mahaksameja86@gmail.com>
2024-10-15 14:53:18 +0530

    env fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@ export const environment = {
   // nullImagePath: 'https://api.blackdad.chooseyourfabric.in/api/v1/null',
 
   //live url
-  // apiUrl: 'https://api.blackdadunfiltered.com/api/v1',
-  // shareUrl: 'https://blackdadunfiltered.com/details',
-  // nullImagePath: "'https://api.blackdadunfiltered.com/api/v1/null"
+  apiUrl: 'https://api.blackdadunfiltered.com/api/v1',
+  shareUrl: 'https://blackdadunfiltered.com/details',
+  nullImagePath: "'https://api.blackdadunfiltered.com/api/v1/null"
 
   //local url
-  apiUrl: 'https://api.blackdad.chooseyourfabric.in/api/v1',
-  shareUrl: 'https://blackdad.xpertmedia.online/details',
-  nullImagePath: 'https://api.blackdad.chooseyourfabric.in/api/v1/null',
+  // apiUrl: 'https://api.blackdad.chooseyourfabric.in/api/v1',
+  // shareUrl: 'https://blackdad.xpertmedia.online/details',
+  // nullImagePath: 'https://api.blackdad.chooseyourfabric.in/api/v1/null',
 };
